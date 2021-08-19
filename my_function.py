06a9aa530e6b28f3104a2241b63a9f5fcf31cc20
ZorkoAnastasiya <asskarra@gmail.com>
2021-08-19 18:18:18 +0300

    pycharm

--- my_function.py
+++ my_function.py
@@ -1,8 +1,64 @@
-def sum_max(*numbers):
-    my_sum = 0
-    my_max = 0
-    for i in range(0, len(numbers)):
-        my_sum += numbers[i]
-        if numbers[i] > numbers[my_max]:
-            my_max = i
-    return my_sum, numbers[my_max]
+import os
+import json
+from fastapi import Request
+from pathlib import Path
+
+
+def sum_cube(num: int):
+    result = sum([x**3 for x in range(1, num+1)])
+    return {"Сумма чисел возведенных в куб": result}
+
+
+def series(start: int, finish: int):
+    list_even_num = [x for x in range(start, finish+1) if x % 2 == 0]
+    list_odd_num = [x for x in range(start, finish+1) if x % 2 != 0]
+    return {
+        "Список четных чисел": list_even_num,
+        "Список нечетных чисел": list_odd_num
+    }
+
+
+def prime_numbers_call(num: int):
+    i = 2
+    while i <= num // 2:
+        if num % i != 0:
+            if i == num // 2:
+                return {"Это число является простым": num}
+            i += 1
+        else:
+            return {"Это число не является простым": num}
+
+
+def gen_random_name():
+    return os.urandom(16).hex()
+
+
+def get_user(request: Request):
+    return request.cookies.get("user")
+
+
+def write_file(filename: str, numbers: dict, user: str):
+    file_path = Path(filename)
+    if file_path.is_file():
+        with open(filename, 'r') as file:
+            old_data = json.load(file)
+            if user in old_data.keys():
+                old_data[user] += numbers[user]
+                new_data = json.dumps(old_data)
+                with open(filename, 'w') as f1:
+                    f1.writelines(new_data)
+            else:
+                with open(filename, 'w') as f2:
+                    old_data[user] = numbers[user]
+                    new_data = json.dumps(old_data)
+                    f2.writelines(new_data)
+    else:
+        with open(filename, 'w') as new_file:
+            new_data = json.dumps(numbers)
+            new_file.writelines(new_data)
+
+
+def read_file(filename: str, user: str):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+        return sum(data[user])
--- task_async.py
+++ task_async.py
@@ -1,8 +1,7 @@
 # coding=utf-8
-from fastapi import FastAPI
-from pydantic import BaseModel
-from my_function import sum_max
-
+import my_function
+from collections import defaultdict
+from fastapi import FastAPI, Body, Request, Response
 
 app = FastAPI()
 
@@ -14,46 +13,29 @@ async def hello():
 
 @app.get('/sum_cube/{num}')
 async def cube(num: int):
-    sum_cube = sum([x**3 for x in range(1, num+1)])
-    return {
-        "Result": sum_cube
-    }
+    return my_function.sum_cube(num)
 
 
 @app.get('/list_num/{start}/{finish}')
-async def numbers(start: int, finish: int):
-    list_num = [x for x in range(start, finish+1) if x % 2 == 0]
-    return {
-        "Список четных чисел": list_num
-    }
+async def numbers_list(start: int, finish: int):
+    return my_function.series(start, finish)
 
 
 @app.get('/prime_numbers/{num}')
 async def prime_numbers(num: int):
-    i = 2
-    while i <= num//2:
-        if num % i != 0:
-            if i == num//2:
-                return {
-                    "Это число является простым": num
-                }
-            i += 1
-        else:
-            return {
-                "Это число не является простым": num
-            }
+    return my_function.prime_numbers_call(num)
  
-        
-class Numbers(BaseModel):
-    numbers: list
-    
 
-@app.get('/calc')
-async def calculation(obj: Numbers):
-    num = obj.numbers
-    sum_value, max_value = sum_max(*num)
-    return {
-        "Task": "Calculation",
-        "Sum of numbers": sum_value,
-        "Maximum value": max_value
-    }
+@app.post('/task/add_number')
+async def handler(request: Request, response: Response, data: str = Body(...)):
+    filename = "Numbers.py"
+    numbers = defaultdict(list)
+    user = my_function.get_user(request) or my_function.gen_random_name()
+    response.set_cookie("user", user)
+    if data == "stop":
+        return my_function.read_file(filename, user)
+    else:
+        assert data.isdigit()
+        numbers[user].append(int(data))
+        my_function.write_file(filename, numbers, user)
+        return {"Введеное число": numbers[user]}
