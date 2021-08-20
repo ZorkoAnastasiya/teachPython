8aacade5c65db58710edd78b76e063317ecf6395
ZorkoAnastasiya <asskarra@gmail.com>
2021-08-20 14:09:32 +0300

    teach pycharm

--- my_function.py
+++ my_function.py
@@ -5,13 +5,13 @@ from pathlib import Path
 
 
 def sum_cube(num: int):
-    result = sum([x**3 for x in range(1, num+1)])
+    result = sum([x ** 3 for x in range(1, num + 1)])
     return {"Сумма чисел возведенных в куб": result}
 
 
 def series(start: int, finish: int):
-    list_even_num = [x for x in range(start, finish+1) if x % 2 == 0]
-    list_odd_num = [x for x in range(start, finish+1) if x % 2 != 0]
+    list_even_num = [x for x in range(start, finish + 1) if x % 2 == 0]
+    list_odd_num = [x for x in range(start, finish + 1) if x % 2 != 0]
     return {
         "Список четных чисел": list_even_num,
         "Список нечетных чисел": list_odd_num
@@ -39,23 +39,22 @@ def get_user(request: Request):
 
 def write_file(filename: str, numbers: dict, user: str):
     file_path = Path(filename)
-    if file_path.is_file():
-        with open(filename, 'r') as file:
-            old_data = json.load(file)
-            if user in old_data.keys():
-                old_data[user] += numbers[user]
-                new_data = json.dumps(old_data)
-                with open(filename, 'w') as f1:
-                    f1.writelines(new_data)
-            else:
-                with open(filename, 'w') as f2:
-                    old_data[user] = numbers[user]
-                    new_data = json.dumps(old_data)
-                    f2.writelines(new_data)
-    else:
+    if not file_path.is_file():
         with open(filename, 'w') as new_file:
             new_data = json.dumps(numbers)
             new_file.writelines(new_data)
+    else:
+        with open(filename, 'r') as file:
+            old_data = json.load(file)
+            with open(filename, 'w') as f1:
+                if user in old_data.keys():
+                    old_data[user] += numbers[user]
+                    new_data = json.dumps(old_data)
+                    f1.writelines(new_data)
+                else:
+                    old_data[user] = numbers[user]
+                    new_data = json.dumps(old_data)
+                    f1.writelines(new_data)
 
 
 def read_file(filename: str, user: str):
